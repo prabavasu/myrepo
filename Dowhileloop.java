00e03827ce7f8fbd15049428d61782f4da60d282
Prabakaran Rengasmy <126864504+prabavasu@users.noreply.github.com>
2023-06-28 12:16:51 +0530

    Update Dowhileloop.java

--- Dowhileloop.java
+++ Dowhileloop.java
@@ -3,12 +3,12 @@ public class Dowhileloop
 public static void main(String[] args)
 {
 int n=5;
-int sum=0;
+int sum=7;
 int i=6;
 do // first print the vales then check the condition is true or false.but condition is false but atlest print one values
 {
 sum +=i;
-}while(i<=5);
+}while(i<=9);
 System.out.println("Sum of n Num:" +sum);
 }
 }
